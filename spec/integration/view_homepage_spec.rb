3b089a35d40716abd51004c0be456e11cbdd84a3
Sherry Seibel <sherry.seibel@gmail.com>
2014-09-25 17:48:00 -0400

    Change name of title and mark 'visible: false'

--- spec/integration/view_homepage_spec.rb
+++ spec/integration/view_homepage_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'View the homepage' do
   scenario 'user sees relevant information' do
     visit root_path
-    expect(page).to have_css 'title', text: 'Todos'
+    expect(page).to have_css 'title', text: 'TodoList', visible: false
   end
 end
