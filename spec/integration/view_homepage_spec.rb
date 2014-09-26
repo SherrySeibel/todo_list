3367b6f68ae59e27487f36cce8529eed76a3e908
Sherry Seibel <sherry.seibel@gmail.com>
2014-09-26 19:18:51 -0400

    Ensure viewing the homepage works

--- spec/integration/view_homepage_spec.rb
+++ spec/integration/view_homepage_spec.rb
@@ -4,5 +4,6 @@ feature 'View the homepage' do
   scenario 'user sees relevant information' do
     visit root_path
     expect(page).to have_css 'title', text: 'TodoList', visible: false
+    expect(page).to have_css '[data-roll="description"]'
   end
 end
